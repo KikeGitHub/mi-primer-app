4f00cad4f76dfb16156abde4c15415471e1294bf
KikeGitHub <kike2013edj@gmail.com>
2023-03-22 13:11:57 -0600

    Mi primer props

--- src/components/HolaMundo.js
+++ src/components/HolaMundo.js
@@ -1,10 +1,9 @@
 import  React from "react";
 
-export default function HolaMundo(){
+export default function HolaMundo(props){
     return(
         <div>
-            <h1>Hola Mundo</h1>
-            <h2>Enrique Duran Jimenez</h2>
+            <h2>Hola Mundo <h1>{props.desc}</h1></h2>
         </div>
     );
 }
