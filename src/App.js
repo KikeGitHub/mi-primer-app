dbf722cabeaf3b589b3d28475a91906034a8b6e3
KikeGitHub <kike2013edj@gmail.com>
2023-03-22 20:59:29 -0600

    Ultimos cambios

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React , { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 import HolaMundo from './components/HolaMundo';
@@ -12,16 +13,29 @@ function App() {
     apellido : "Duran"
   }
 
+  const [count, setCount] = useState(0);
+
+  const increaseCounter = () =>{
+    setCount(count + 1);
+  };
+
+
   const saludarFn = name => {
     console.log("Hola " + name);
   }
+
+  useEffect( () =>{
+    console.log("contador " + count);
+  }, [count]
+  );
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <HolaMundo userInfo={userInfo}/>
-        <AdiosMundo userInfo={userInfo} saludarFn={saludarFn}/>
+        <h3>contador = {count}</h3>
+        <button onClick={increaseCounter}>Contador</button>
       </header>
     </div>
   );
